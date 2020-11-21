eac1d91fc98bd95269669d28fcdae376e139e0c5
Erhunosa Osagie-Amayo <erhunosaosagie-amayo@10-197-131-201.sds.oregonstate.edu>
2020-11-21 15:00:57 -0800

    changed createPool

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,13 @@
-
-var mysql = require('mysql');
+var mysql = require("mysql");
 var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_osagieae',
-  password        : '7996',
-  database        : 'cs340_osagieae'
+  connectionLimit: 1000,
+  connectTimeout: 60 * 60 * 1000,
+  acquireTimeout: 60 * 60 * 1000,
+  timeout: 60 * 60 * 1000,
+  host: "classmysql.engr.oregonstate.edu",
+  user: "cs340_osagieae",
+  password: "7996",
+  database: "cs340_osagieae",
 });
 
 module.exports.pool = pool;
